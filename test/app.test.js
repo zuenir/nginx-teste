e7df8796fe5bcdc0e18f29cd87c5b5dc1e81e982
Zuenir Claudio de Lima Vemba <zuenirlima@gmail.com>
2025-03-30 23:57:19 +0000

    Add Test Stage

--- test/app.test.js
+++ test/app.test.js
@@ -2,43 +2,43 @@ const request = require('supertest');
 const chai = require('chai');
 const expect = chai.expect;
 const sinon = require('sinon');
-const app = require('../server.js'); // Importando o app do arquivo app.js
+const app = require('../server.js');
 
-describe('Testes da aplicação Express', function(){
-    it('Deve responder com status 200 na rota "/"', function(done){
+describe('Testes da aplicação Express', function() {
+    it('Deve responder com status 200 na rota "/"', function(done) {
         request(app)
             .get('/')
-            .expect(200,done);
+            .expect(200, done);  // Esse `done` garante que o teste é concluído corretamente.
     });
 
-    it('Deve retornar um arquivo estático de imagens na rota "/images', function(done){
+    it('Deve retornar um arquivo estático de imagens na rota "/images"', function(done) {
         request(app)
-            .get('/images/devsecops.png')  // Altere para uma imagem existente em sua pasta "images"
+            .get('/images/devsecops.png')
             .expect('Content-Type', /png/)
-            .expect(200,done);
+            .expect(200, done);
     });
 
-    it('Deve servir um arquivo HTML na rota "/"', function(done){
+    it('Deve servir um arquivo HTML na rota "/"', function(done) {
         request(app)
             .get('/')
             .expect('Content-Type', /html/)
             .expect(200)
-            .end((err,res)=>{
-                if(err) return done(err)
-                expect(res.text).to.include('<!DOCTYPE html>');// Verifica se o HTML retornado contém a tag <html>
+            .end((err, res) => {
+                if (err) return done(err);
+                expect(res.text).to.include('<!DOCTYPE html>');
                 done();
             });
     });
 
-    it('Deve registrar o nome da aplicação no console ao acessar a rota "/"', function(done){
-        const consoleLogSpy = sinon.spy(console, 'log'); // Spy para monitorar console.log
+    it('Deve registrar o nome da aplicação no console ao acessar a rota "/"', function(done) {
+        const consoleLogSpy = sinon.spy(console, 'log');
 
         request(app)
             .get('/')
             .end((err, res) => {
                 expect(consoleLogSpy.calledWith('Request served by TestApp')).to.be.true;
-                consoleLogSpy.restore(); // Restaurar o console.log original
+                consoleLogSpy.restore();
                 done();
             });
     });
-})
+});
