83c06762d7385f787a8c2d9e680f6bb61c4a5000
Zuenir Claudio de Lima Vemba <zuenirlima@gmail.com>
2025-04-01 19:44:11 +0000

    Add Junit Report for Jest Test Result

--- Jenkinsfile
+++ Jenkinsfile
@@ -36,4 +36,9 @@ pipeline {
             }
         }
     }
+    post {
+        always {
+            junit 'test-results/junit.xml'
+        }
+    }
 }
---
+++ jest.config.js
@@ -0,0 +1,12 @@
+module.exports={
+    reporters:[
+        "default",
+        [
+            "jest-junit",
+            {
+                outputDirectory: "test-results",
+                outputName: "junit.xml",
+            }
+        ]
+    ],
+};
