27aa5ea70dd6661f03fa8c50cd4aecb8d9e60ec0
Zuenir Claudio de Lima Vemba <zuenirlima@gmail.com>
2025-04-04 12:25:13 +0000

    Remove Cypress and Update package

--- cypress.config.js
+++
@@ -1,9 +0,0 @@
-// cypress.config.js
-module.exports = {
-    e2e: {
-      baseUrl: 'http://localhost:3000', // Substitua com o endereço da sua aplicação
-      supportFile: 'cypress/support/e2e.js', // Caminho para o arquivo de suporte
-      specPattern: 'cypress/e2e/**/*.{js,ts,jsx,tsx}', // Onde seus testes estarão localizados
-    },
-  };
-  
